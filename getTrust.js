f4a618b0fe58399eb499265cbe1caf321047d2c5
Ryusei-M-G <ryusei.morishima1023@gmail.com>
2025-11-08 23:55:34 +0900

    feat: add getTrust.js file

---
+++ getTrust.js
@@ -0,0 +1,3 @@
+export const getTrust = async(req,res) =>{
+    
+}
--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.post('/api/verify',verify);
 // app.get('/api/getVrcId',getVrcID);
-// app.get('/api/getTrust',getTrust);
+app.get('/api/getTrust',getTrust);
 
 app.listen(3000,()=>{
     console.log('server is running');
