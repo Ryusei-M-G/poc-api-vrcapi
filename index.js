e74f6e8815e4bd12c1d1b8e182748359b3750960
Ryusei-M-G <ryusei.morishima1023@gmail.com>
2025-11-08 21:34:22 +0900

    fix: getTrust

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.post('/api/verify',verify);
 app.get('/api/getVrcId',getVrcID);
-app.get('/api/getTrust',)
+app.get('/api/getTrust',getTrust);
 
 app.listen(3000,()=>{
     console.log('server is running');
