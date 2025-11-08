5b6e310974e251a0df0698475c5f1ebf69c1ec66
Ryusei-M-G <ryusei.morishima1023@gmail.com>
2025-11-08 21:33:42 +0900

    feat: endpoint

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ import express from "express";
 
 const app = express();
 
+
+
+app.post('/api/verify',verify);
+app.get('/api/getVrcId',getVrcID);
+app.get('/api/getTrust',)
+
 app.listen(3000,()=>{
     console.log('server is running');
 })
