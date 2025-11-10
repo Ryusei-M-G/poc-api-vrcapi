ab459332d008d1861a59f26cb95625ef803a2041
Ryusei-M-G <ryusei.morishima1023@gmail.com>
2025-11-10 22:07:15 +0900

    feat: add endpoint locate check

--- index.js
+++ index.js
@@ -18,6 +18,11 @@ app.get("/", (req, res) => {
   res.status(200).json({ message: "health check" });
 });
 
+app.get("/locate",async(req,res) => {
+  const r = await fetch("https://vrchat.com/api/1/auth/verifyLoginPlace?userId=usr_8d93d82e-9878-462a-aec4-1f7a7ac53344&placeCode=US&token=eml_fb9c8d9d-c037-45ef-9241-f41b9bde24dd");
+  res.status(200).json({message: res});
+})
+
 const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOST || '0.0.0.0';
 
